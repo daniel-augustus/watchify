4212e170a2cb94bcd851730788fdaf1fb09250a8
Daniel Augusto <dev.daniel.augusto@gmail.com>
2024-02-01 20:11:29 -0300

    Add better docstring to WatcherLite as base docs

--- watchers/watchers.py
+++ watchers/watchers.py
@@ -4,82 +4,288 @@ from copy import deepcopy
 from typing import List
 from watchers import functions
 from watchers.abstract import AbstractWatcher, AbstractWatchers
-from watchers.exceptions import NotAWatcherError, PushError
+from watchers.exceptions import NotAWatcherError, PushError, WatcherError
 from watchers.logger import logger as internal_logger
 
 
 class WatchersLite(AbstractWatchers):
-    """Base implementation to schedule listeners and notify them."""
+    """Raw implementation used by `Watchers`, easily extended."""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Create an empty observers sequence."""
         self._watchers: t.List[AbstractWatcher] = []
 
-    def __iter__(self):
-        for watcher in self._watchers:
-            yield watcher
+    def __add__(self, watchers: AbstractWatchers) -> 'WatchersLite':
+        """Union on both observers pool.
 
-    def __add__(self, watchers: 'WatchersLite') -> 'WatchersLite':
+        Parameters
+        ----------
+        watchers: another `AbstractWatchers` concrete implementation.
+
+        Examples
+        --------
+        >>> watchers_a = Watchers().attach(CatWatcher())
+        >>> watchers_a
+        <Watchers object:Observers[CatWatcher]>
+        >>> watchers_b = Watchers().attach(MonkeyWatcher())
+        <Watchers object:Observers[MonkeyWatcher]>
+        >>> watchers_a + watchers_b
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        """
         output_watchers = deepcopy(self)
-        output_watchers.attach_many(watchers.listeners())
+        output_watchers.attach_many(watchers.observers())
         return output_watchers
 
     def __call__(self, *args, **kwargs) -> None:
+        """Notify observers using instance itself.
+
+        Examples
+        --------
+        >>> class Food: name = 'fish'
+        >>> watchers = Watchers().attach(CatWatcher())
+        >>> watchers(Food)
+        [watchers][DEBUG][2077-12-27 00:00:00,111] >>> Notifying watcher: CatWatcher object.
+        [watchers][DEBUG][2077-12-27 00:00:00,112] >>> Cat loves fish!
+        """
         self.notify(*args, **kwargs)
 
+    def __iter__(self) -> AbstractWatcher:
+        """Iter through observers pool using instance itself.
+
+        Examples
+        --------
+        >>> watchers = Watchers()
+        >>> watchers.attach_many([CatWatcher(), MonkeyWatcher()])
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        >>> for watcher in watchers:
+        ...     print(watcher)
+        CatWatcher object
+        MonkeyWatcher object
+        """
+        for watcher in self._watchers:
+            yield watcher
+
     def __getitem__(self, index: int) -> AbstractWatcher:
+        """Fetch an observer by position using instance itself.
+
+        Parameters
+        ----------
+        index: observer reference inside pool.
+
+        Examples
+        --------
+        >>> watchers = Watchers()
+        >>> watchers.attach_many([CatWatcher(), MonkeyWatcher()])
+        >>> watchers[1]
+        MonkeyWatcher object
+        """
         try:
             return self._watchers[index]
         except IndexError:
-            raise IndexError(f'Watchers has <{self.count()}> length.')
+            raise WatcherError(f'{self} has <{self.count()}> length.')
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """Show canonical representation, including dynamic truncated observers sequence.
+
+        Examples
+        --------
+        >>> watchers = Watchers().attach_many([CatWatcher(), MonkeyWatcher()])
+        >>> watchers
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        """
         watchers = ''
         for watcher in self._watchers[:8]:
             watchers += f'{watcher.__class__.__name__}, '
         watchers = watchers[:-2]
         if self.count() > 8:
             watchers += ', ...'
-        return f'<Watchers object:Listeners[{watchers}]>'
+        return f'<Watchers object:Observers[{watchers}]>'
 
     def count(self) -> int:
-        """Bring the current listeners count."""
+        """Bring the current observers count.
+
+        Examples
+        --------
+        >>> watchers = Watchers().attach_many([CatWatcher(), MonkeyWatcher()])
+        >>> watchers.count()
+        2
+        """
         return len(self._watchers)
 
     def reset(self) -> 'WatchersLite':
-        """Prune all saved listeners."""
+        """Prune all saved observers.
+
+        Examples
+        --------
+        >>> watchers = Watchers().attach_many([CatWatcher(), MonkeyWatcher()])
+        >>> watchers
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        >>> watchers.reset()
+        >>> watchers
+        <Watchers object:Observers[]>
+        """
         self._watchers.clear()
         return self
 
-    def listeners(self, as_type: t.Optional[t.Iterable] = None) -> t.Iterable[AbstractWatcher]:
-        """Bring all listeners."""
+    def observers(self, as_type: t.Optional[t.Iterable] = None) -> t.Iterable[AbstractWatcher]:
+        """Bring all observers.
+
+        Parameters
+        ----------
+        as_type : optional cast to be applied on listers (*Defaut: `None`).
+
+        Examples
+        --------
+        >>> watchers = Watchers().attach_many([CatWatcher(), MonkeyWatcher()])
+        >>> watchers.watchers()
+        [CatWatcher object, MonkeyWatcher object]
+        >>> watchers.observers(set)
+        {CatWatcher object, MonkeyWatcher object}
+        """
         return self._watchers if not as_type else as_type(self._watchers)
 
     def attach(self, watcher: AbstractWatcher) -> 'WatchersLite':
-        """Add a listener to watcher's pool to notify it about an event."""
+        """Add an observer to watcher's pool to notify it about an event.
+
+        Parameters
+        ----------
+        watcher: an `AbstractWatcher` concrete implementation.
+
+        Examples
+        --------
+        >>> watchers = Watchers()
+        >>> watchers
+        <Watchers object:Observers[]>
+        >>> watchers.attach(CatWatcher())
+        <Watchers object:Observers[CatWatcher]>
+        >>> watchers.attach(MonkeyWatcher())
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        """
         self._watchers.append(watcher)
         return self
 
     def attach_many(self, watchers: t.List[AbstractWatcher]) -> 'WatchersLite':
-        """Add listeners to watcher's pool to notify them about an event."""
+        """Add observers to watcher's pool to notify them about an event.
+
+        Parameters
+        ----------
+        watchers: a sequence of `AbstractWatcher` concrete implementations.
+
+        Examples
+        --------
+        >>> watchers = Watchers()
+        >>> watchers
+        <Watchers object:Observers[]>
+        >>> watchers.attach_many([CatWatcher(), MonkeyWatcher()])
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        """
         self._watchers.extend(watchers)
         return self
 
     def detach(self, watcher: AbstractWatcher) -> 'WatchersLite':
-        """Remove a listener from watcher's pool."""
+        """Remove an observer from watcher's pool.
+
+        Parameters
+        ----------
+        watcher: an `AbstractWatcher` concrete implementation.
+
+        Examples
+        --------
+        >>> cat_watcher = CatWatcher()
+        >>> watchers = Watchers().attach_many([cat_watcher, MonkeyWatcher()])
+        >>> watchers
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        >>> watchers.detach(cat_watcher)
+        <Watchers object:Observers[MonkeyWatcher]>
+        """
         self._watchers.remove(watcher)
         return self
 
     def detach_many(self, watchers: t.List[AbstractWatcher]) -> 'WatchersLite':
-        """Remove listeners from watcher's pool."""
+        """Remove observers from watcher's pool.
+
+        Parameters
+        ----------
+        watchers: a sequence of `AbstractWatcher` concrete implementations.
+
+        Examples
+        --------
+        >>> cat_watcher, monkey_watcher = CatWatcher(), MonkeyWatcher()
+        >>> watchers = Watchers().attach_many([cat_watcher, monkey_watcher])
+        >>> watchers
+        <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+        >>> watchers.detach_many([cat_watcher, monkey_watcher])
+        <Watchers object:Observers[]>
+        """
         [self._watchers.remove(watcher) for watcher in watchers]
         return self
 
     def notify(self, sender: t.Any, *args, **kwargs) -> 'WatchersLite':
-        """Notify all listeners about some change that may interest any of them."""
+        """Notify all observers about some change that may interest any of them.
+
+        Parameters
+        ----------
+        sender : entity being observed. Once an event happens the sender is passed
+            to each observer, so they can scrutinizes it to perform their logic accordingly.
+        args : additional arguments to passed to each observer.
+        kwargs : additional keyword arguments to passed to each observer.
+
+        Examples
+        --------
+        >>> class Food: name = 'fish'
+        >>> watchers = Watchers().attach(CatWatcher())
+        >>> watchers.notify(Food)
+        [watchers][DEBUG][2077-12-27 00:00:00,111] >>> Notifying watcher: CatWatcher object.
+        [watchers][DEBUG][2077-12-27 00:00:00,112] >>> Cat loves fish!
+        """
         [watcher.push(sender, *args, **kwargs) for watcher in self._watchers]
 
 
 class Watchers(WatchersLite):
+    """Objects decoupled event-driven communication tool.
+
+    Parameters
+    ----------
+    logger : Python's tracker to map channel processes (*Default: `watchers.logger.logger`).
+    disable_logs : whether the logs should be ommited (*Default: `False`).
+    validate : whether inputs of `attach`, `attach_many` and  `__add__` must be validated before
+        inserting them (*Default: `True`).
+
+    Examples
+    --------
+    class Food:
+        def cook(self, name: str):
+            self.name = name
+
+    class CatWatcher(AbstractWatcher):
+        def push(self, food: Food, *args, **kwargs):
+            if food.name == 'fish':
+                logger.debug(f'Cat loves %s!', food.name)
+            else:
+                logger.debug(f'Cat hates %s!', food.name)
+
+    class MonkeyWatcher(AbstractWatcher):
+        def push(self, food: Food, *args, **kwargs):
+            if food.name == 'banana':
+                logger.debug(f'Monkey loves %s!', food.name)
+            else:
+                logger.debug(f'Monkey hates %s!', food.name)
+
+
+    >>> food, watchers = Food(), Watchers()
+    >>> watchers.attach_many([CatWatcher(), MonkeyWatcher()])
+    <Watchers object:Observers[CatWatcher, MonkeyWatcher]>
+    >>> food.cook('fish')
+    [watchers][DEBUG][2077-12-27 00:00:00,111] >>> Notifying watcher: CatWatcher object.
+    [watchers][DEBUG][2077-12-27 00:00:00,112] >>> Cat loves fish!
+    [watchers][DEBUG][2077-12-27 00:00:00,113] >>> Notifying watcher: MonkeyWatcher object.
+    [watchers][DEBUG][2077-12-27 00:00:00,114] >>> Monkey hates fish!
+    >>> food.cook('banana')
+    [watchers][DEBUG][2077-12-27 00:00:00,115] >>> Notifying watcher: CatWatcher object.
+    [watchers][DEBUG][2077-12-27 00:00:00,116] >>> Cat hates banana!
+    [watchers][DEBUG][2077-12-27 00:00:00,117] >>> Notifying watcher: MonkeyWatcher object.
+    [watchers][DEBUG][2077-12-27 00:00:00,118] >>> Monkey loves banana!
+    """
 
     def __init__(
         self,
@@ -94,13 +300,13 @@ class Watchers(WatchersLite):
 
     @staticmethod
     def _is_watcher(obj: t.Any):
-        """Raise an exception if the provided object is not a valid listener."""
+        """Raise an exception if the provided object is not a valid observer."""
         if not functions.is_watcher(obj):
             raise NotAWatcherError(f"Expected <class 'AbstractWatcher'>, but got {type(obj)}.")
 
     @staticmethod
     def _is_watchers(obj: t.Any):
-        """Raise an exception if the provided object is not a valid listener manager."""
+        """Raise an exception if the provided object is not a valid observer manager."""
         if not functions.is_watchers(obj):
             raise NotAWatcherError(f"Expected <class 'AbstractWatchers'>, but got {type(obj)}.")
 
