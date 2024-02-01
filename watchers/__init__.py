27a82abf09cc4c20039fef5c085c593ce4c0d3f6
Daniel Augusto <dev.daniel.augusto@gmail.com>
2024-02-01 18:00:45 -0300

    Add Watchers and WatchersSpy specializations

---
+++ watchers/__init__.py
@@ -0,0 +1,11 @@
+from watchers.abstract import AbstractWatcher, AbstractWatchers
+from watchers.watchers import Watchers, WatchersLite
+from watchers.spies import WatchersSpy
+
+__all__ = [
+    'AbstractWatcher',
+    'AbstractWatchers',
+    'Watchers',
+    'WatchersLite',
+    'WatchersSpy',
+]
